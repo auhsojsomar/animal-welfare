091d49214dd899bbbe10bcb093ef7902357d58d7
auhsojsomar <auhsojsomar@gmail.com>
2018-10-26 05:18:53 +0800

    commit

--- admin/login/function.php
+++ admin/login/function.php
@@ -1,10 +1,12 @@
 <?php
+    session_start();
     include '../../core/config.php';
     $user = $_POST['username'];
     $pass = $_POST['password'];
     $sql = mysqli_query($connect,"SELECT * FROM user_tbl WHERE username = '$user' AND password = '$pass' AND user_type = 'admin'");
     $row = mysqli_num_rows($sql);
     if($row > 0){
+        $_SESSION['user'] = $user;
         echo "Success";
     }
     else{
